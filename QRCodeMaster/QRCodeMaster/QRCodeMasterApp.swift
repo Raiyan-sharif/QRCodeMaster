9ed2c29321e6b494260dc7f5197d881a8ee3260e
RaiyanSharifSynesisIT <raiyan.sharif@synesisit.info>
2026-04-11 17:45:38 +0600

    init project

---
+++ QRCodeMaster/QRCodeMaster/QRCodeMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeMasterApp.swift
+//  QRCodeMaster
+//
+//  Created by Raiyan Sharif on 11/4/26.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
